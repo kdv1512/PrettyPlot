00ed4f45f74e44aa1c80d29a31c2d565a624491c
Kyle Vu <11ringsandcounting@Kyles-MacBook.local>
2020-09-26 21:20:50 -0500

    first commit

--- js/pretty.js
+++ js/pretty.js
@@ -67,27 +67,61 @@ var makeTranslateString = function(x,y)
 //graphDim is an object that describes the width and height of the graph area.
 //margins is an object that describes the space around the graph
 //xScale and yScale are the scales for the x and y scale.
-var drawAxes = function(graphDim,margins,
-                         xScale,yScale)
+var drawAxes = function(graph, margins, xScale, yScale)
 {
-   
- 
+   var xAxis = d3.axisBottom(xScale);
+    var yAxis = d3.axisLeft(yScale);
+    
+    d3.select("svg")
+    .append("g")
+    .classed("axis", true)
+    .attr("transform", makeTranslateString(margins.left, margins.top+graph.height))
+    .call(xAxis)
+    
+    d3.select("svg")
+    .append("g")
+    .classed("axis",true)
+    .attr("transform", makeTranslateString(margins.left, 
+             margins.top))
+    .call(yAxis)
 }
 
 
 //graphDim -object that stores dimensions of the graph area
 //margins - objedct that stores the size of the margins
-var drawLabels = function(graphDim,margins)
+var drawLabels = function(graph, margins, screen)
 {
+    var labels = d3.select("svg")
+    .append("g")
+    .classed("labels", true)
     
+    labels.append("text")
+    .text("Trump Support")
+    .classed("title", true)
+    .attr("text-anchor", "middle")
+    .attr("x", margins.left+(graph.width/2))
+    .attr("y", margins.top+10)
+    
+    labels.append("text")
+    .text("Percentage of White voters")
+    .classed("label", true)
+    .attr("text-anchor", "middle")
+    .attr("x", margins.left+(graph.width/2))
+    .attr("y", 580)
+    
+    labels.append("g")
+    .attr("transform", makeTranslateString(20, margins.top+(graph.height/2)))
+    .append("text")
+    .text("Percentage of Trump votes")
+    .classed("label",true)
+    .attr("text-anchor","middle")
+    .attr("transform", "rotate(90)")
 }
 
 
 var drawLegend = function(graphDim,margins)
 {
-    
- 
-   var categories = [
+ var categories = [
        {
            class:"lessCollege",
            name:"Less College"
@@ -98,26 +132,56 @@ var drawLegend = function(graphDim,margins)
        }
     ]
 
+var legend = d3.select("svg")
+    .append("g")
+    .classed("legend",true)
+    .attr("transform", makeTranslateString(90,70) )
 
+var entries = legend.selectAll("g")
+        .data(categories.map(function(category)
+                            {
+            return category.name
+        }))
+        .enter()
+        .append("g")
+        .classed("legendEntry",true)
+        .attr("fill", function(category)
+              { 
+            if(category == "Less College")
+                {return "red"}
+            if(category == "High unemployment")
+                {return "blue"}
+        })
+        .attr("transform",function(categories,index)
+              {
+                return "translate(0,"+index*20+")";
+              })
+              
+        entries.append("rect")
+                .attr("width", 10)
+                .attr("height",10)
     
-    
-    
+        entries.append("text")
+                .text(function(name){return name})
+                .attr("x",15)
+                .attr("y",10)   
 }
+
 
 //sets up several important variables and calls the functions for the visualization.
 var initGraph = function(counties)
 {
     //size of screen
-    var screen = {width:800,height:600}
+    var screen = {width:1300,height:600}
     //how much space on each side
-    var margins = {left:30,right:20,top:20,bottom:30}
+    var margins = {left:70,right:200,top:20,bottom:40}
     
     
     
     var graph = 
         {
             width:screen.width-margins.left-margins.right,
-            height:screen.height - margins.top-margins.bottom
+            height:screen.height - margins.top-margins.bottom -20
         }
     console.log(graph);
     
